b7fd7a0a9e631a1a1af6100278dd0ecb065d1e8e
mary.mounir <m.zekrie@gmail.com>
2024-07-10 05:01:09 +0300

    fav jobs from db

--- lib/view/pages/jobs_all_2_screen.dart
+++ lib/view/pages/jobs_all_2_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../control/cubit/fav_job/app_cubit.dart';
 import '../builder_item/all_job_2_item.dart';
-
 
 class JobsAll2Screen extends StatelessWidget {
 
